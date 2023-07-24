ca8b7fa650f61f41d36d54b3ec0c32a1cc584657
drasi <drasi69@gmail.com>
2023-07-24 17:32:32 +0200

    Initial commit

---
+++ src/test/java/hu/drasi/lifegame/LifegameApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.drasi.lifegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
